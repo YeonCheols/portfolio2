9792585188740bcb0304f5998ba7d13f7f859c80
SungStack <s9292909@gmail.com>
2025-04-16 11:22:07 +0900

    [add]: github token 업데이트

--- src/common/constant/github.ts
+++ src/common/constant/github.ts
@@ -7,8 +7,8 @@ export const GITHUB_ACCOUNTS = [
   //   is_active: true,
   // },
   {
-    username: 'aulianza',
-    token: process.env.GITHUB_READ_USER_TOKEN_PERSONAL,
+    username: process.env.GITHUB_USER_NAME || '',
+    token: process.env.GITHUB_ACCESS_TOKEN,
     endpoint: '/api/github?type=personal',
     type: 'personal',
     is_active: true,
