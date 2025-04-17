dbdbb57df74293250cd19e2250eb41c93b8350e3
SungStack <s9292909@gmail.com>
2025-04-17 10:46:04 +0900

    [fix]:  dashboard 페이지 문구수정

--- src/modules/dashboard/components/Contributions/Contributions.tsx
+++ src/modules/dashboard/components/Contributions/Contributions.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import { BsGithub as GithubIcon } from 'react-icons/bs';
-import useSWR from 'swr';
+import Link from "next/link";
+import { BsGithub as GithubIcon } from "react-icons/bs";
+import useSWR from "swr";
 
-import SectionHeading from '@/common/components/elements/SectionHeading';
-import SectionSubHeading from '@/common/components/elements/SectionSubHeading';
-import { fetcher } from '@/services/fetcher';
+import SectionHeading from "@/common/components/elements/SectionHeading";
+import SectionSubHeading from "@/common/components/elements/SectionSubHeading";
+import { fetcher } from "@/services/fetcher";
 
-import Calendar from './Calendar';
-import Overview from './Overview';
+import Calendar from "./Calendar";
+import Overview from "./Overview";
 
 type ContributionsProps = {
   username: string;
@@ -22,29 +22,30 @@ const Contributions = ({ username, endpoint }: ContributionsProps) => {
     data?.contributionsCollection?.contributionCalendar;
 
   return (
-    <section className='flex flex-col gap-y-2'>
+    <section className="flex flex-col gap-y-2">
       <SectionHeading
-        title='Contributions'
-        icon={<GithubIcon className='mr-1' />}
+        title="Contributions"
+        icon={<GithubIcon className="mr-1" />}
       />
       <SectionSubHeading>
-        <p className='dark:text-neutral-400'>
-          My contributions from last year on github.
+        <p className="dark:text-neutral-400">
+          {contributionCalendar?.totalContributions} contributions in the last
+          year
         </p>
         <Link
           href={`https://github.com/${username}`}
-          target='_blank'
+          target="_blank"
           passHref
-          className='font-code text-sm text-neutral-400 hover:text-neutral-700 dark:text-neutral-600 hover:dark:text-neutral-400'
+          className="font-code text-sm text-neutral-400 hover:text-neutral-700 dark:text-neutral-600 hover:dark:text-neutral-400"
         >
           @{username}
         </Link>
       </SectionSubHeading>
 
-      {!data && <div className='dark:text-neutral-400'>No Data</div>}
+      {!data && <div className="dark:text-neutral-400">No Data</div>}
 
       {data && (
-        <div className='space-y-3'>
+        <div className="space-y-3">
           <Overview data={contributionCalendar} />
           <Calendar data={contributionCalendar} />
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { NextPage } from 'next';
-import { NextSeo } from 'next-seo';
+import { NextPage } from "next";
+import { NextSeo } from "next-seo";
 
-import Container from '@/common/components/elements/Container';
-import Home from '@/modules/home';
+import Container from "@/common/components/elements/Container";
+import Home from "@/modules/home";
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <NextSeo title='Ryan Aulia - Personal Website' />
-      <Container data-aos='fade-up'>
+      <NextSeo title="Yeon Cheol - 포트폴리오" />
+      <Container data-aos="fade-up">
         <Home />
       </Container>
     </>
