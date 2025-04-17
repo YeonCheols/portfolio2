36edbcf6f43d3e76b50ed1be5201e2915ac97ab9
SungStack <s9292909@gmail.com>
2025-04-17 15:57:47 +0900

    [fix]: mdx 파일 불러오기 에러

--- src/common/libs/mdx.ts
+++ src/common/libs/mdx.ts
@@ -1,19 +1,20 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import path from 'path';
-import { remark } from 'remark';
-import remarkGfm from 'remark-gfm';
-import remarkMdx from 'remark-mdx';
-import remarkParse from 'remark-parse';
-
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+// import unified from "unified";
+import remarkMdx from "remark-mdx";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
 interface MdxFileProps {
   slug: string;
   frontMatter: Record<string, unknown>;
   content: string;
 }
 
+var unified = require("unified");
+
 export const loadMdxFiles = (slug: string): MdxFileProps[] => {
-  const dirPath = path.join(process.cwd(), 'src', 'contents', 'learn', slug);
+  const dirPath = path.join(process.cwd(), "src", "contents", "learn", slug);
 
   if (!fs.existsSync(dirPath)) {
     return [];
@@ -21,16 +22,22 @@ export const loadMdxFiles = (slug: string): MdxFileProps[] => {
 
   const files = fs.readdirSync(dirPath);
 
+  console.log("files : ", files);
+
   const contents = files.map((file) => {
     const filePath = path.join(dirPath, file);
-    const source = fs.readFileSync(filePath, 'utf-8');
+    const source = fs.readFileSync(filePath, "utf-8");
     const { content, data } = matter(source);
 
-    const mdxCompiler = remark().use(remarkParse).use(remarkGfm).use(remarkMdx);
-    const mdxContent = mdxCompiler.processSync(content).toString();
+    // const mdxCompiler = unified()
+    //   .use(remarkParse)
+    //   .use(remarkGfm as any)
+    //   .use(remarkMdx);
+
+    const mdxContent = content.toString();
 
     return {
-      slug: file.replace('.mdx', ''),
+      slug: file.replace(".mdx", ""),
       frontMatter: data,
       content: mdxContent,
     };
@@ -40,8 +47,8 @@ export const loadMdxFiles = (slug: string): MdxFileProps[] => {
 };
 
 export const getMdxFileCount = (slug: string) => {
-  const dirPath = path.join(process.cwd(), 'src', 'contents', 'learn', slug);
+  const dirPath = path.join(process.cwd(), "src", "contents", "learn", slug);
   const files = fs.readdirSync(dirPath);
-  const mdxFiles = files.filter((file) => file.endsWith('.mdx'));
+  const mdxFiles = files.filter((file) => file.endsWith(".mdx"));
   return mdxFiles.length;
 };
