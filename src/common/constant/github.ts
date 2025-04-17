f6548bd0d27e9add39c5f08bd4703d78abde7d66
SungStack <s9292909@gmail.com>
2025-04-17 14:38:36 +0900

    [fix]: wakatime api 정보 수정

--- src/common/constant/github.ts
+++ src/common/constant/github.ts
@@ -1,7 +1,7 @@
 export const GITHUB_ACCOUNTS = [
   {
-    username: process.env.GITHUB_USER_NAME || "",
-    token: process.env.GITHUB_ACCESS_TOKEN,
+    username: process.env.NEXT_PUBLIC_GITHUB_USER_NAME || "",
+    token: process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN,
     endpoint: "/api/github?type=personal",
     type: "personal",
     is_active: true,
