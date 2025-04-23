b3de8f4f1ce54d95f7954e175b45a5ec83650ba2
SungStack <s9292909@gmail.com>
2025-04-23 16:25:50 +0900

    fix : project prisma 쿼리 수정

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -13,7 +13,7 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-    const response = await prisma.projects.findMany();
+    const response = await prisma.projects.findMany({});
     res.status(200).json({ status: true, data: response });
   } catch (error) {
     res.status(200).json({ status: false, error: error });
