6ede5c9c0aa5015d47c2c8000b81bef21345e5fe
SungStack <s9292909@gmail.com>
2025-04-27 20:38:52 +0900

    modify : meta title 변경

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -5,14 +5,14 @@ import Container from "@/common/components/elements/Container";
 import PageHeading from "@/common/components/elements/PageHeading";
 // import Chat from "@/modules/chat";
 
-const PAGE_TITLE = "Guestbook";
+const PAGE_TITLE = "게스트북";
 const PAGE_DESCRIPTION =
   "Leave whatever you like to say, suggestions, questions or anything!";
 
 const GuestBookPage: NextPage = () => {
   return (
     <>
-      <NextSeo title={`${PAGE_TITLE} - Yeon Cheol`} />
+      <NextSeo title={`연철s - ${PAGE_TITLE}`} />
       <Container data-aos="fade-up">
         <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
         {/* <Chat /> */}
