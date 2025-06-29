01de0af2caf0b8e1556e3b555cd5da9feaaa0b83
SungStack <s9292909@gmail.com>
2025-06-29 21:20:01 +0900

    docs: playgroud meta title

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import Container from "@/common/components/elements/Container";
 import Playground from "@/modules/playground";
 
-const PAGE_TITLE = "js playground";
+const PAGE_TITLE = "playground";
 
 const playground: NextPage = () => {
   return (
