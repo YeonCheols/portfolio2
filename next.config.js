2a95d98d497417598b1d0a612eac9a691c51c3e8
SungStack <s9292909@gmail.com>
2025-07-18 13:42:35 +0900

    fix: prisma 코드 제거

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      // HMR 관련 설정
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
