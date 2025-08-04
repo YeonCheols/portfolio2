b81e57e402e2721a3b4984bb88b598f603b65c7c
SungStack <s9292909@gmail.com>
2025-08-05 08:45:36 +0900

    fix: 메일 발송폼 수정

--- src/modules/contact/components/Contact.tsx
+++ src/modules/contact/components/Contact.tsx
@@ -1,18 +1,18 @@
-import Breakline from '@/common/components/elements/Breakline';
+import Breakline from "@/common/components/elements/Breakline";
 
-import BookACall from './BookACall';
-import ContactForm from './ContactForm';
-import SocialMediaList from './SocialMediaList';
+import BookACall from "./BookACall";
+import ContactForm from "./ContactForm";
+import SocialMediaList from "./SocialMediaList";
 
 const Contact = () => {
   return (
-    <section className='space-y-6'>
+    <section className="space-y-6">
       <SocialMediaList />
       <Breakline />
-      <BookACall />
+      {/* <BookACall /> */}
       <Breakline />
-      <div className='space-y-5'>
-        <h3 className='text-lg font-medium'>Or send me a message</h3>
+      <div className="space-y-5">
+        <h3 className="text-lg font-medium">Or send me a message</h3>
         <ContactForm />
       </div>
     </section>
