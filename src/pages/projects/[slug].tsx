a294fef5ac8962a03bc4d326bf24359d800648e1
성연철 <seong-yeoncheol@MI-2-ycseng.local>
2025-11-04 08:15:32 +0900

    test: 디버깅 테스트

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,39 +1,39 @@
 import { GetServerSideProps, NextPage } from "next";
-import { NextSeo } from "next-seo";
-import axios from "axios";
-import dynamic from "next/dynamic";
+// import { NextSeo } from "next-seo";
+// import axios from "axios";
+// import dynamic from "next/dynamic";
 
-import BackButton from "@/common/components/elements/BackButton";
-import Container from "@/common/components/elements/Container";
-import PageHeading from "@/common/components/elements/PageHeading";
-import { useStacks } from "@/common/hooks/useStacks";
+// import BackButton from "@/common/components/elements/BackButton";
+// import Container from "@/common/components/elements/Container";
+// import PageHeading from "@/common/components/elements/PageHeading";
+// import { useStacks } from "@/common/hooks/useStacks";
 import { ProjectResponse } from "@docs/api";
 
 // 동적 import로 외부 패키지 컴포넌트 로드
-const ProjectPreviewDetail = dynamic(
-  () =>
-    import("@yeoncheols/portfolio-core-ui").then((mod) => mod.ProjectPreview),
-  {
-    ssr: false,
-    loading: () => <div>Loading...</div>,
-  },
-);
+// const ProjectPreviewDetail = dynamic(
+//   () =>
+//     import("@yeoncheols/portfolio-core-ui").then((mod) => mod.ProjectPreview),
+//   {
+//     ssr: false,
+//     loading: () => <div>Loading...</div>,
+//   },
+// );
 
 interface ProjectsDetailPageProps {
   project: ProjectResponse;
 }
 
 const ProjectsDetailPage = ({ project }: ProjectsDetailPageProps) => {
-  const PAGE_TITLE = project?.title;
-  const PAGE_DESCRIPTION = project?.description;
+  // const PAGE_TITLE = project?.title;
+  // const PAGE_DESCRIPTION = project?.description;
 
-  const canonicalUrl = `https://www.ycseng.com/projects/${project?.slug}`;
+  // const canonicalUrl = `https://www.ycseng.com/projects/${project?.slug}`;
 
-  const { StackIcons } = useStacks();
+  // const { StackIcons } = useStacks();
 
   return (
     <>
-      <NextSeo
+      {/* <NextSeo
         title={`연철s 프로젝트 - ${project?.title}`}
         description={project?.description}
         canonical={canonicalUrl}
@@ -57,38 +57,38 @@ const ProjectsDetailPage = ({ project }: ProjectsDetailPageProps) => {
         <BackButton url="/projects" />
         <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
         <ProjectPreviewDetail data={project} stackIcons={StackIcons} />
-      </Container>
+      </Container> */}
     </>
   );
 };
 
 export default ProjectsDetailPage;
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  if (typeof params?.slug === "undefined") {
-    return {
-      redirect: {
-        destination: "/404",
-        permanent: false,
-      },
-    };
-  }
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   if (typeof params?.slug === "undefined") {
+//     return {
+//       redirect: {
+//         destination: "/404",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  const { data, status } = await axios.get(
-    `${process.env.API_URL}/project/${params?.slug}`,
-  );
+//   const { data, status } = await axios.get(
+//     `${process.env.API_URL}/project/${params?.slug}`,
+//   );
 
-  if (!data || status !== 200) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: {
-      project: JSON.parse(JSON.stringify(data)),
-    },
-  };
-};
+//   if (!data || status !== 200) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
+//   return {
+//     props: {
+//       project: JSON.parse(JSON.stringify(data)),
+//     },
+//   };
+// };
